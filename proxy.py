53f7c5f8124591fce8611ddaf89833146f3dd86d
Marcelo Fernandez <marcelo@thirdpartytrust.com>
2022-05-05 12:33:30 -0300

    Cleaning unneeded code, adding spanish comments

--- proxy.py
+++ proxy.py
@@ -1,109 +1,77 @@
 #!/usr/bin/env python3
-from http.server import BaseHTTPRequestHandler,HTTPServer
-import argparse, os, random, sys, requests
-
+import requests
+import urllib3
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from socketserver import ThreadingMixIn
-import threading
 
+""" HTTP Proxy de ejemplo para la cursada de Teleinformática y Redes """
 
-hostname = 'en.wikipedia.org'
+urllib3.disable_warnings()
 
-def merge_two_dicts(x, y):
-    return x | y
+hostname = 'es.wikipedia.org'
+port = 8080
 
-def set_header():
+def get_headers() -> dict:
+    """ Devolver el diccionario de headers que quiero enviar forzadamente al servidor real """
     headers = {
         'Host': hostname
     }
 
     return headers
 
+
 class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
     protocol_version = 'HTTP/1.0'
+
     def do_HEAD(self):
-        self.do_GET(body=False)
+        self.do_GET(body=False)  # la respuesta a un HEAD es un GET pero sin body
         return
-        
+
     def do_GET(self, body=True):
         sent = False
         try:
-            url = 'https://{}{}'.format(hostname, self.path)
-            req_header = self.parse_headers()
+            url = 'https://{}{}'.format(hostname, self.path)  # armo el string con la URL real a la cual invocar
+            req_header = dict(self.headers)  # En principio, seteamos los mismos headers del cliente
+            req_header.update(get_headers())  # Actualiza los elementos del diccionario "pisando" los actuales
 
-            print(req_header)
-            print(url)
-            resp = requests.get(url, headers=merge_two_dicts(req_header, set_header()), verify=False)
+            print(url)  # Imprimo la petición que voy a hacer al servidor
+            resp = requests.get(url, headers=req_header, verify=False, stream=True)
             sent = True
 
-            self.send_response(resp.status_code)
-            self.send_resp_headers(resp)
-            msg = resp.text
-            if body:
-                self.wfile.write(msg.encode(encoding='UTF-8',errors='strict'))
+            self.send_response(resp.status_code)  # Enviar el status code de la petición a nuestro cliente
+            self.send_resp_headers(resp)  # Enviar los headers de la petición a nuestro cliente
+            if body:  # Si nos hicieron una petición GET, escribimos el mensaje obtenido al cliente
+                msg = resp.text  # Copio el body de la petición a una variable
+                self.wfile.write(msg.encode(encoding=resp.encoding or resp.apparent_encoding or 'utf-8'))
             return
         finally:
             if not sent:
-                self.send_error(404, 'error trying to proxy')
-
-    def do_POST(self, body=True):
-        sent = False
-        try:
-            url = 'https://{}{}'.format(hostname, self.path)
-            content_len = int(self.headers.getheader('content-length', 0))
-            post_body = self.rfile.read(content_len)
-            req_header = self.parse_headers()
-
-            resp = requests.post(url, data=post_body, headers=merge_two_dicts(req_header, set_header()), verify=False)
-            sent = True
-
-            self.send_response(resp.status_code)
-            self.send_resp_headers(resp)
-            if body:
-                self.wfile.write(resp.content)
-            return
-        finally:
-            if not sent:
-                self.send_error(404, 'error trying to proxy')
-
-    def parse_headers(self):
-        req_header = {}
-        for line in self.headers:
-            line_parts = [o.strip() for o in line.split(':', 1)]
-            if len(line_parts) == 2:
-                req_header[line_parts[0]] = line_parts[1]
-        return req_header
+                self.send_error(404, 'error haciendo proxy de la peticion')
 
     def send_resp_headers(self, resp):
         respheaders = resp.headers
-        print ('Response Header')
         for key in respheaders:
-            if key not in ['Content-Encoding', 'Transfer-Encoding', 'content-encoding', 'transfer-encoding', 'content-length', 'Content-Length']:
-                print (key, respheaders[key])
-                self.send_header(key, respheaders[key])
-        self.send_header('Content-Length', len(resp.content))
-        self.end_headers()
+            # Limpio estos headers de la petición al servidor
+            if key not in ['Content-Encoding', 'Transfer-Encoding', 'content-encoding', 'transfer-encoding',
+                           'content-length', 'Content-Length', 'Date', 'date', 'Server', 'server']:
+                self.send_header(key, respheaders[key])  # Envío el header del servidor al cliente
+        self.send_header('Content-Length', len(resp.content))  # Envío el tamaño de la respuesta al cliente
+        self.end_headers()  # Fin de headers
 
-def parse_args(argv=sys.argv[1:]):
-    parser = argparse.ArgumentParser(description='Proxy HTTP requests')
-    parser.add_argument('--port', dest='port', type=int, default=9999,
-                        help='serve HTTP requests on specified port (default: random)')
-    parser.add_argument('--hostname', dest='hostname', type=str, default='en.wikipedia.org',
-                        help='hostname to be processd (default: en.wikipedia.org)')
-    args = parser.parse_args(argv)
-    return args
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-    """Handle requests in a separate thread."""
+    """ Permite manejar las peticiones del navegador en hilos de ejecución separados """
+    pass
 
-def main(argv=sys.argv[1:]):
-    global hostname
-    args = parse_args(argv)
-    hostname = args.hostname
-    print('http server is starting on {} port {}...'.format(args.hostname, args.port))
-    server_address = ('127.0.0.1', args.port)
+
+def main():
+    global hostname, port
+    print(f'El servidor HTTP se está iniciando en {hostname} port {port}...')
+    server_address = ('127.0.0.1', port)
     httpd = ThreadedHTTPServer(server_address, ProxyHTTPRequestHandler)
-    print('http server is running as reverse proxy')
+    print('El Servidor HTTP se está ejecutando como HTTP Proxy Reverso')
     httpd.serve_forever()
+
 
 if __name__ == '__main__':
     main()
